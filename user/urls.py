5db4270f0790e9f651aa7a41c64524598fd614ef
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-03 17:13:59 +0500

    .

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserProfileDetailView
+from .views import UserProfileDetailView, UserProfileCreateView
 
 
 urlpatterns = [
+    path('user-profile/create/', UserProfileCreateView.as_view(), name='user-profile-create'),
     path('user-profile/', UserProfileDetailView.as_view(), name='profile_detail_update'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,6 +3,16 @@ from .models import UserProfile
 from .serializers import UserProfileSerializer
 
 
+# Create a new user profile (if it doesn't exist)
+class UserProfileCreateView(generics.CreateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically associate the authenticated user with the profile
+        serializer.save(user=self.request.user)
+
+
 # Ensure the user is authenticated and retrieve their own profile
 class UserProfileDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
