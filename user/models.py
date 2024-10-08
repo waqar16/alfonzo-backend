63368610a907167fcf373ff737d56e9da245349c
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-08 17:46:59 +0500

    some changes by waqar

--- user/models.py
+++ user/models.py
@@ -34,6 +34,8 @@ class UserDocument(models.Model):
     base64_content = models.TextField()
     pdf_url = models.URLField(null=True, blank=True)
     template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
 
