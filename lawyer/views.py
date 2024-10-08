5e538b5d36e9e2e6c83d96115ce759bf4c03cf22
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-08 14:05:41 +0500

    some changes by waqar

--- lawyer/views.py
+++ lawyer/views.py
@@ -1,10 +1,23 @@
 from rest_framework import generics, permissions
 from .serializers import LawyerProfileSerializer
+from .models import Lawyerprofile
 
 
+# Create a new lawyer profile (if it doesn't exist)
+class UserProfileCreateView(generics.CreateAPIView):
+    serializer_class = LawyerProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically associate the authenticated user with the profile
+        serializer.save(user=self.request.user)
+
+
+# Retrieve, update or delete a lawyer profile       
 class LawyerProfileDetailUpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = LawyerProfileSerializer
     permission_classes = [permissions.AllowAny]
 
     def get_object(self):
-        return self.request.user.username
+        # Return the user profile of the authenticated user
+        return Lawyerprofile.objects.get(user=self.request.user)
