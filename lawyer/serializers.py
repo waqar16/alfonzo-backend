8fe03077c61cc049f431ac9ec0ef154812395d24
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-09 14:47:53 +0500

    .

--- lawyer/serializers.py
+++ lawyer/serializers.py
@@ -34,7 +34,7 @@ class LawyerDocumentSerializer(serializers.ModelSerializer):
 class UserQuerySerializer(serializers.ModelSerializer):
     class Meta:
         model = UserQuery
-        fields = ['id', 'lawyer', 'message']
+        fields = ['id', 'user', 'lawyer', 'message']
 
     def create(self, validated_data):
         # Automatically set the user when creating a new query
