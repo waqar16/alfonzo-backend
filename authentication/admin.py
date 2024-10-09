a02efc848f6a91cbecd491b6e93e35c26d353da9
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-09 13:44:57 +0500

    .

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
 
 User = get_user_model()
 
-
-class UserAdmin(BaseUserAdmin):
-    def save_model(self, request, obj, form, change):
-        if not change:  # This means the user is being created
-            obj.set_password(obj.password)  # Hash the password
-        super().save_model(request, obj, form, change)
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
