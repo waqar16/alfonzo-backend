755f6bd1c4f0840200bde6755c92d986c05b7b5f
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-09 14:10:25 +0500

    .

--- lawyer/admin.py
+++ lawyer/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import LawyerProfile
+from .models import LawyerProfile, LawyerDocument, UserQuery
 
 admin.site.register(LawyerProfile)
+admin.site.register(LawyerDocument)
+admin.site.register(UserQuery)
