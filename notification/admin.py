823e1957e2dc14e7e674ad2795f22fea6c0726e5
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-14 16:07:12 +0500

    .

--- notification/admin.py
+++ notification/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import Notification
 
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'message', 'created_at')    
-
-    def has_add_permission(self, request):
-        return False
+admin.site.register(Notification)
