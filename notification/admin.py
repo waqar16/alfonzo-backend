cc4e6c90f1a6e702655dde221f92c44b59705ea0
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-14 17:33:29 +0500

    .

--- notification/admin.py
+++ notification/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Notification
 
-admin.site.register(Notification)
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'message', 'created_at', 'is_read')
+    list_filter = ('is_read', 'created_at', 'user')
+    search_fields = ('message', 'user__username')
+    actions = ['mark_as_read']
+
+    def mark_as_read(self, request, queryset):
+        queryset.update(is_read=True)
+    mark_as_read.short_description = 'Mark selected notifications as read'
