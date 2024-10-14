f29d6acc4826040a81ce4e2cc11b4433b49d0ca8
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-14 16:13:25 +0500

    .

--- notification/models.py
+++ notification/models.py
@@ -6,6 +6,7 @@ User = get_user_model()
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=255)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
