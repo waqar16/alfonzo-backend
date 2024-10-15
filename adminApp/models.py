6d30fe6965bba71879d0a68056899dd50ce54367
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-15 14:22:32 +0500

    .

--- adminApp/models.py
+++ adminApp/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     sub_categories = models.JSONField(null=True, blank=True)
 
     def __str__(self):
