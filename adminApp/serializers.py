8b5cc5b691c5734a53663a1baf30097653a23d2a
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-15 17:01:00 +0500

    .

--- adminApp/serializers.py
+++ adminApp/serializers.py
@@ -2,14 +2,17 @@ from rest_framework import serializers
 from .models import Template, Category
 
 
-class TemplateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Template
-        fields = '__all__'
-
-
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ['id', 'name', 'sub_categories']
+    
+    
+class TemplateSerializer(serializers.ModelSerializer):
+    
+    category = CategorySerializer()
+    
+    class Meta:
+        model = Template
+        fields = ['id', 'name', 'category', 'questions', 'content']
