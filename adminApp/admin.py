65c95bda9307e1d9bfcd703d7ef432864166abd0
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-15 13:50:25 +0500

    .

--- adminApp/admin.py
+++ adminApp/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from .models import Template, Category
 
-@admin.register(Template)
-class TemplateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'questions', 'content')
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'sub_categories')
+admin.register(Template)
+admin.register(Category)
