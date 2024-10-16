fd4a4e8fa5aec11cc03fb8c647498501d4063003
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-16 17:09:37 +0500

    .

--- adminApp/serializers.py
+++ adminApp/serializers.py
@@ -1,122 +1,26 @@
-# from rest_framework import serializers
-# from .models import Template, Category, SubCategory
-
-
-# class SubCategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = SubCategory
-#         fields = ['id', 'name']
-
-
-# class CategorySerializer(serializers.ModelSerializer):
-#     sub_categories = SubCategorySerializer(many=True)
-
-#     class Meta:
-#         model = Category
-#         fields = ['id', 'name', 'sub_categories']
-
-#     def create(self, validated_data):
-#         sub_categories_data = validated_data.pop('sub_categories', [])
-#         category = Category.objects.create(**validated_data)
-#         for sub_category_data in sub_categories_data:
-#             SubCategory.objects.create(category=category, **sub_category_data)
-#         return category
-
-#     def update(self, instance, validated_data):
-#         sub_categories_data = validated_data.pop('sub_categories', [])
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.save()
-
-#         # Clear old subcategories and create new ones
-#         instance.sub_categories.all().delete()
-#         for sub_category_data in sub_categories_data:
-#             SubCategory.objects.create(category=instance, **sub_category_data)
-
-#         return instance
-
-
-# class TemplateSerializer(serializers.ModelSerializer):
-#     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-#     sub_category = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all(), source='SubCategory')
-
-#     class Meta:
-#         model = Template
-#         fields = ['id', 'name', 'category', 'sub_category', 'questions', 'content', 'created_at']
-
-#     def create(self, validated_data):
-#         return Template.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.category = validated_data.get('category', instance.category)
-#         instance.SubCategory = validated_data.get('sub_category', instance.SubCategory)
-#         instance.questions = validated_data.get('questions', instance.questions)
-#         instance.content = validated_data.get('content', instance.content)
-#         instance.save()
-#         return instance
-
-
-from rest_framework import serializers
-from .models import Template, Category, SubCategory
-
-
-class SubCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubCategory
-        fields = ['id', 'name']
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    sub_categories = SubCategorySerializer(many=True)
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'sub_categories']
-
-    def create(self, validated_data):
-        sub_categories_data = validated_data.pop('sub_categories', [])
-        category = Category.objects.create(**validated_data)
-        for sub_category_data in sub_categories_data:
-            SubCategory.objects.create(category=category, **sub_category_data)
-        return category
-
-    def update(self, instance, validated_data):
-        sub_categories_data = validated_data.pop('sub_categories', [])
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-
-        # Clear old subcategories and create new ones
-        instance.sub_categories.all().delete()
-        for sub_category_data in sub_categories_data:
-            SubCategory.objects.create(category=instance, **sub_category_data)
-
-        return instance
-
-
 class TemplateSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    sub_category = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all(), source='SubCategory')
+    sub_category = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all())  # Removed source
 
     class Meta:
         model = Template
         fields = ['id', 'name', 'category', 'sub_category', 'questions', 'content', 'created_at']
 
     def create(self, validated_data):
-        # Extract category and sub_category IDs
         category_id = validated_data.pop('category')
-        sub_category_id = validated_data.pop('sub_category', None)  # Correcting to 'sub_category'
+        sub_category_id = validated_data.pop('sub_category', None)
 
-        # Retrieve the Category and SubCategory instances
+        # Use category_id and sub_category_id directly since they are integers (PKs)
         validated_data['category'] = Category.objects.get(id=category_id)
         if sub_category_id:
-            validated_data['SubCategory'] = SubCategory.objects.get(id=sub_category_id)
+            validated_data['sub_category'] = SubCategory.objects.get(id=sub_category_id)
 
         return Template.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.category = validated_data.get('category', instance.category)
-        instance.SubCategory = validated_data.get('sub_category', instance.SubCategory)  # Correcting to 'sub_category'
+        instance.sub_category = validated_data.get('sub_category', instance.sub_category)  # Fixed to sub_category
         instance.questions = validated_data.get('questions', instance.questions)
         instance.content = validated_data.get('content', instance.content)
         instance.save()
@@ -124,14 +28,13 @@ class TemplateSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        # Include complete category and sub_category objects
         representation['category'] = {
             'id': instance.category.id,
             'name': instance.category.name,
             'sub_categories': [{'id': sub.id, 'name': sub.name} for sub in instance.category.sub_categories.all()]
         }
         representation['sub_category'] = {
-            'id': instance.SubCategory.id,
-            'name': instance.SubCategory.name
-        } if instance.SubCategory else None
+            'id': instance.sub_category.id,
+            'name': instance.sub_category.name
+        } if instance.sub_category else None
         return representation
