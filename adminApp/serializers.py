7721e018ac100919346eb483dd13b7da92131af5
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-16 13:40:37 +0500

    .

--- adminApp/serializers.py
+++ adminApp/serializers.py
@@ -40,45 +40,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class TemplateSerializer(serializers.ModelSerializer):
-    # Nested category and subcategory serializers
-    category = CategorySerializer()
-    sub_category = SubCategorySerializer()  # Use lowercase 'sub_category' for consistency
 
     class Meta:
         model = Template
-        fields = ['id', 'name', 'category', 'sub_category', 'questions', 'content']
-
-    def create(self, validated_data):
-        # Extract the nested category data
-        category_data = validated_data.pop('category')
-        sub_category_data = validated_data.pop('sub_category')  # Extract subcategory data
-
-        # Create or get the category object
-        category, _ = Category.objects.get_or_create(**category_data)
-
-        # Create or get the subcategory object
-        sub_category, _ = SubCategory.objects.get_or_create(**sub_category_data)
-
-        # Create the Template with the associated category and subcategory
-        template = Template.objects.create(category=category, sub_category=sub_category, **validated_data)
-        return template
-
-    def update(self, instance, validated_data):
-        # Extract the nested category data
-        category_data = validated_data.pop('category')
-        sub_category_data = validated_data.pop('sub_category')  # Extract subcategory data
-
-        # Update or create the category
-        category, _ = Category.objects.get_or_create(**category_data)
-
-        # Update or create the subcategory
-        sub_category, _ = SubCategory.objects.get_or_create(**sub_category_data)
-
-        # Update the Template instance
-        instance.category = category
-        instance.sub_category = sub_category  # Update the subcategory
-        instance.name = validated_data.get('name', instance.name)
-        instance.questions = validated_data.get('questions', instance.questions)
-        instance.content = validated_data.get('content', instance.content)
-        instance.save()
-        return instance
+        fields = ['id', 'name', 'category', 'SubCategory', 'questions', 'content']
