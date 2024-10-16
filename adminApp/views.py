44d96abf265593edc62a0e124d60092e6dedbf53
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-16 17:28:06 +0500

    .

--- adminApp/views.py
+++ adminApp/views.py
@@ -96,20 +96,11 @@ class TemplateListCreateView(generics.ListCreateAPIView):
     """
     queryset = Template.objects.all()
     serializer_class = TemplateSerializer
-    pagination_class = None  # Initially set to None
+    pagination_class = None
 
-    def get_paginator(self):
-        """
-        Override the default paginator to allow disabling pagination via query param.
-        """
-        paginate = self.request.query_params.get('paginate', 'true').lower()
-        if paginate == 'false':
-            return None  # Disable pagination
-        return super().get_paginator()  # Use default pagination
-    
     def get_queryset(self):
         queryset = super().get_queryset()
-        
+   
         # Get query parameters
         category_name = self.request.query_params.get('category', None)
         sub_category_name = self.request.query_params.get('sub_category', None)
