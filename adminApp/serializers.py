047394f77c3d1a7c2b6878a1f3cd68387940df54
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-16 17:13:17 +0500

    .

--- adminApp/serializers.py
+++ adminApp/serializers.py
@@ -95,27 +95,21 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class TemplateSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    sub_category = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all())  # Removed source
+    sub_category = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all())
 
     class Meta:
         model = Template
         fields = ['id', 'name', 'category', 'sub_category', 'questions', 'content', 'created_at']
 
     def create(self, validated_data):
-        category_id = validated_data.pop('category')
-        sub_category_id = validated_data.pop('sub_category', None)
-
-        # Use category_id and sub_category_id directly since they are integers (PKs)
-        validated_data['category'] = Category.objects.get(id=category_id)
-        if sub_category_id:
-            validated_data['sub_category'] = SubCategory.objects.get(id=sub_category_id)
-
+        # Directly use validated data without changing it
         return Template.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
+        # Update instance directly from validated_data
         instance.name = validated_data.get('name', instance.name)
-        instance.category = validated_data.get('category', instance.category)
-        instance.sub_category = validated_data.get('sub_category', instance.sub_category)  # Fixed to sub_category
+        instance.category = validated_data.get('category', instance.category)  # Expecting a Category ID
+        instance.sub_category = validated_data.get('sub_category', instance.sub_category)  # Expecting a SubCategory ID
         instance.questions = validated_data.get('questions', instance.questions)
         instance.content = validated_data.get('content', instance.content)
         instance.save()
