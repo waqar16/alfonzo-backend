c25f4778aea0b139e632523dc3446baa55dc2d2e
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-18 17:58:02 +0500

    addes requirements.txt

--- adminApp/serializers.py
+++ adminApp/serializers.py
@@ -65,13 +65,13 @@ from lawyer.models import LawyerDocument
 class UserDocumentsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDocument
-        field = '__all__'
+        fields = '__all__'
   
 
 class LawyerDocumentsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = LawyerDocument
-        field = '__all__'
+        fields = '__all__'
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
