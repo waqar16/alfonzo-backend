7043afffcb1af80fbda15f4474fb8d11b7e6a896
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-10-18 16:58:32 +0500

    addes requirements.txt

--- adminApp/urls.py
+++ adminApp/urls.py
@@ -12,7 +12,7 @@ from .views import (
     TemplateListCreateView,
     TemplateDetailView,
     CategoryListCreateView,
-    DocumentsViewSet,
+    DocumentsListView,
     # CategoryDetailView,
 )
 
@@ -29,5 +29,5 @@ urlpatterns = [
     path('analytics/most-used-templates-by-lawyer/', MostUsedTemplatesByLawyer.as_view(), name='most-used-templates-by-lawyer'),
     path('templates/', TemplateListCreateView.as_view(), name='template-list-create'),
     path('templates/<int:pk>/', TemplateDetailView.as_view(), name='template-detail'),
-    path('all-created-documents/', DocumentsViewSet.as_view({'get': 'list'}), name='admin-documents'),
+    path('all-created-documents/', DocumentsListView.as_view({'get': 'list'}), name='admin-documents'),
 ]
